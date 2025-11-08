684d741f46f30a77e033a1e60f8390780ee84135
Vitor Mandira <vg.mandira@gmail.com>
2025-11-07 22:55:34 -0300

    Refactor App component with navigation and loading

--- App.js
+++ App.js
@@ -1,33 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { useFonts, Inter_300Light, Inter_700Bold } from '@expo-google-fonts/inter';
-import * as SplashScreen from 'expo-splash-screen';
-import { useCallback, useEffect, useState } from 'react';
-import ChooseProfile from './src/screens/chooseProfile/index';
+import { useFonts, Inter_300Light,  Inter_700Bold} from '@expo-google-fonts/inter';
+import AppLoading from 'expo-app-loading';
+import Login from './src/screens/userStudent/login/index';
+import Escolha from './src/screens/chooseProfile/index';
+import Home from './src/screens/userStudent/home/index';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import TelaTeorica from "./src/screens/userStudent/molds/gameQuiz/explanationScreen/index";
+import TelaJogo from "./src/screens/userStudent/molds/gameQuiz/gameScreen/index";
+import TelaBird from "./src/screens/userStudent/molds/flappyQuiz/gameScreen/index";
+import TelaTeoricaNarrativa from "./src/screens/userStudent/molds/narrative/explanationScreen/index";
+import TelaNarrativa from "./src/screens/userStudent/molds/narrative/StoryPlayScreen/index";
 
-// Impede que a Splash Screen desapareça automaticamente
-SplashScreen.preventAutoHideAsync();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  //ta importando as fontes que vamos usar
   const [fontsLoaded] = useFonts({
     Inter_300Light,
     Inter_700Bold,
   });
 
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-
-  if (!fontsLoaded) {
-    return null; // Fica mostrando a splash screen até carregar
+  //se não carregar a fonte vai retornar AppLoading
+  if(!fontsLoaded) {
+    return <AppLoading/>;
   }
 
+
+  
+
+  //conteudo principal
   return (
-    <View style={styles.container} onLayout={onLayoutRootView}>
-      <ChooseProfile/>
-    </View>
+
+     <NavigationContainer>
+      <Stack.Navigator initialRouteName="StoryIntro">
+        <Stack.Screen
+          name="TelaTeorica"
+          component={TelaTeorica}
+          options={{ title: "Explicação do Plano" }}
+        />
+        <Stack.Screen
+          name="TelaJogo"
+          component={TelaJogo}
+          options={{ title: "Jogo Quiz" }}
+        />
+
+        <Stack.Screen
+          name="flappyBird"
+          component={TelaBird}
+          options={{ title: "Jogo Interativo" }}
+        />
+
+        <Stack.Screen
+          name="StoryIntro"
+          component={TelaTeoricaNarrativa}
+          options={{ title: "Jogo " }}
+        />
+        <Stack.Screen
+          name="StoryPlay"
+          component={TelaNarrativa}
+          options={{ title: "Jogo I" }}
+        />
+
+
+      </Stack.Navigator>
+
+    </NavigationContainer>
+
+    // <View style={styles.container}>
+    //   <Home/>
+    // </View>
   );
 }
 
